a8cbf1e65eb2b95b18bd61dc0a32708d98be7edb
Riya kaushik <riyakaushik6410@gmail.com>
2025-07-18 14:16:59 +0530

    feat: add testimonial section and Ai Tool section with internal navigation

--- client/src/assets/asset.js
+++ client/src/assets/asset.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg"
 import favicon from "./favicon.jpg";
 import user_group from "./user_group.png";
 import star_icon from "./star_icon.svg";
-import star_dull_icon from "./star_dull_icon.svg";
+import star_dull_icon from "./star_dull_icon.jpg";
 import profile_img_1 from "./profile_img_1.jpg";
 import arrow_icon from "./arrow_icon.svg";
 import { SquarePen, Hash, Image, Eraser, Scissors, FileText } from 'lucide-react'
@@ -26,42 +26,42 @@ export const AiToolsData = [
         description: 'Generate high-quality, engaging articles on any topic with our AI writing technology.',
         Icon: SquarePen,
         bg: { from: '#3588F2', to: '#0BB0D7' },
-        path: '/ai/write-article'
+        path: '/ai/WriteArticles'
     },
     {
         title: 'Blog Title Generator',
         description: 'Find the perfect, catchy title for your blog posts with our AI-powered generator.',
         Icon: Hash,
         bg: { from: '#B153EA', to: '#E549A3' },
-        path: '/ai/blog-titles'
+        path: '/ai/BlogTitle'
     },
     {
         title: 'AI Image Generation',
         description: 'Create stunning visuals with our AI image generation tool, Experience the power of AI ',
         Icon: Image,
         bg: { from: '#20C363', to: '#11B97E' },
-        path: '/ai/generate-images'
+        path: '/ai/GenerateImage'
     },
     {
         title: 'Background Removal',
         description: 'Effortlessly remove backgrounds from your images with our AI-driven tool.',
         Icon: Eraser,
         bg: { from: '#F76C1C', to: '#F04A3C' },
-        path: '/ai/remove-background'
+        path: '/ai/RemoveBackground'
     },
     {
         title: 'Object Removal',
         description: 'Remove unwanted objects from your images seamlessly with our AI object removal tool.',
         Icon: Scissors,
         bg: { from: '#5C6AF1', to: '#427DF5' },
-        path: '/ai/remove-object'
+        path: '/ai/RemoveObjects'
     },
     {
         title: 'Resume Reviewer',
         description: 'Get your resume reviewed by AI to improve your chances of landing your dream job.',
         Icon: FileText,
         bg: { from: '#12B7AC', to: '#08B6CE' },
-        path: '/ai/review-resume'
+        path: '/ai/ReviewResume'
     }
 ]
 
