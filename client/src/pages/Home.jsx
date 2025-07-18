71debedf7be1ecabfe6a7d5bbe625e8ee542d644
Riya kaushik <riyakaushik6410@gmail.com>
2025-07-18 14:16:59 +0530

    feat: add testimonial section and Ai Tool section with internal navigation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero'
+import AiTools from '../components/AiTools';
+import Testimonials from '../components/Testimonials';
 
 const Home = () => {
   return (
   <>
    <Navbar/>
    <Hero/>
+   <AiTools/>
+   <Testimonials/>
   </>
   )
 }
