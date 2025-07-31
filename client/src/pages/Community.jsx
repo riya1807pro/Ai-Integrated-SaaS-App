a1a8bac1578d42cdb519132734b7fe66be9a8fb9
Riya kaushik <riyakaushik6410@gmail.com>
2025-07-31 11:44:27 +0530

    feat: finalized community page

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import {useUser} from "@clerk/clerk-react"
 import {dummyPublishedCreationData} from '../assets/asset'
+import { Heart } from 'lucide-react'
 
 const Community = () => {
 
@@ -25,10 +26,19 @@ const Community = () => {
             key={index}>
               <img src={creationItem.content} alt="" className='w-full h-full object-cover rounded-lg' />
 
-              <div>
+              <div className='absolute bottom-0 left-3 top-0 right-0 flex gap-2 
+              items-end justify-end group-hover:justify-between p-3 text-white rounded-lg
+              group-hover:bg-gradient-to-b from-transparent to-black/80 '>
                 <p className='text-sm hidden group-hover:block'>
                   {creationItem.prompt}
                 </p>
+
+                <div className='flex gap-1 items-center'>
+                  <p>{creationItem.likes.length} </p>
+                    <Heart className={`min-w- 5 h-5 hover:scale-110 cursor-pointer
+                      ${creationItem.likes.includes(user.id) ? 'text-red-500 fill-red-500' : 'text-white' }`}
+                    />
+                </div>
               </div>
             </div>
           )
