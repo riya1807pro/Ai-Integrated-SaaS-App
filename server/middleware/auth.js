a83e18e25ef4632aa4e52527fa611f44972735e7
Riya kaushik <riyakaushik6410@gmail.com>
2025-07-31 12:55:02 +0530

    Setup: Initialized backend with middleware, database connection & basic configuration

---
+++ server/middleware/auth.js
@@ -0,0 +1,29 @@
+import { clerkClient } from "@clerk/express";
+import e from "express";
+
+//middle ware to check user dat and premium status
+
+export const auth = async(req, res, next)=>{
+    try {
+        const {userId , auth} = await req.auth();
+        const hasPremium = await has({plan: "premium"});
+
+        const {user} = await clerkClient.users.getUser(userId);
+
+        if(!hasPremium && user.privateMetadata.free_usage) {
+            req.free_usage = user.privateMetadata.free_usage;
+        }else{
+            await clerkClient.users.updateUserMetadata(userId, {
+                free_usage : 0
+            })
+            req.free_usage = 0;
+        }
+        req.plan = hasPremium ? "premium" : "free";
+        next();
+    } catch (error) {
+        req.json({
+            success: false,
+            message: error.message || "An error occurred during authentication"
+        })
+    }
+}
